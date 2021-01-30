bc0a3ea0775d8b88f37caf0913f69546395967a1
Danil <dddsaltykov@gmail.com>
2021-01-30 14:25:52 +0500

    Доделал aside, сверстал поиск, но недоделал всплывающее меню при клике на выбор категории

--- dist/js/main.js
+++ dist/js/main.js
@@ -166,14 +166,15 @@ var da = new DynamicAdapt("max");
 da.init();
 var burger = document.querySelector('.icon-menu');
 var menu = document.querySelector('.menu__body');
-var menuParents = document.querySelectorAll('.menu-page__link_parent');
-var submenuItems = document.querySelectorAll('.submenu-page__item');
+var menuParents = document.querySelectorAll('.menu-page__parent');
 
 var _loop2 = function _loop2(index) {
   var menuParent = menuParents[index];
   menuParent.addEventListener('mouseenter', function () {
-    var submenuNumber = parseInt(menuParent.getAttribute('data-item'));
-    submenuItems[submenuNumber].classList.add('_active');
+    menuParent.classList.add('_active');
+  });
+  menuParent.addEventListener('mouseleave', function () {
+    menuParent.classList.remove('_active');
   });
 };
 
@@ -185,3 +186,80 @@ burger.addEventListener('click', function () {
   burger.classList.toggle('_active');
   menu.classList.toggle('_active');
 });
+var menuPageBurger = document.querySelector('.menu-page__burger');
+var menuPageBody = document.querySelector('.menu-page__body');
+menuPageBurger.addEventListener('click', function () {
+  menuPageBurger.classList.toggle('_active');
+
+  _slideToggle(menuPageBody);
+}); //SlideToggle
+
+var _slideUp = function _slideUp(target) {
+  var duration = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 500;
+  target.style.transitionProperty = 'height, margin, padding';
+  target.style.transitionDuration = duration + 'ms';
+  target.style.height = target.offsetHeight + 'px';
+  target.offsetHeight;
+  target.style.overflow = 'hidden';
+  target.style.height = 0;
+  target.style.paddingTop = 0;
+  target.style.paddingBottom = 0;
+  target.style.marginTop = 0;
+  target.style.marginBottom = 0;
+  window.setTimeout(function () {
+    target.style.display = 'none';
+    target.style.removeProperty('height');
+    target.style.removeProperty('padding-top');
+    target.style.removeProperty('padding-bottom');
+    target.style.removeProperty('margin-top');
+    target.style.removeProperty('margin-bottom');
+    target.style.removeProperty('overflow');
+    target.style.removeProperty('transition-duration');
+    target.style.removeProperty('transition-property');
+    target.classList.remove('_slide');
+  }, duration);
+};
+
+var _slideDown = function _slideDown(target) {
+  var duration = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 500;
+  target.style.removeProperty('display');
+  var display = window.getComputedStyle(target).display;
+  if (display === 'none') display = 'block';
+  target.style.display = display;
+  var height = target.offsetHeight;
+  target.style.overflow = 'hidden';
+  target.style.height = 0;
+  target.style.paddingTop = 0;
+  target.style.paddingBottom = 0;
+  target.style.marginTop = 0;
+  target.style.marginBottom = 0;
+  target.offsetHeight;
+  target.style.transitionProperty = 'height, margin, padding';
+  target.style.transitionDuration = duration + 'ms';
+  target.style.height = height + 'px';
+  target.style.removeProperty('padding-top');
+  target.style.removeProperty('padding-bottom');
+  target.style.removeProperty('margin-top');
+  target.style.removeProperty('margin-bottom');
+  window.setTimeout(function () {
+    target.style.removeProperty('height');
+    target.style.removeProperty('overflow');
+    target.style.removeProperty('transition-duration');
+    target.style.removeProperty('transition-property');
+    target.classList.remove('_slide');
+  }, duration);
+};
+
+var _slideToggle = function _slideToggle(target) {
+  var duration = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 500;
+
+  if (!target.classList.contains('_slide')) {
+    target.classList.add('_slide');
+
+    if (window.getComputedStyle(target).display === 'none') {
+      return _slideDown(target, duration);
+    } else {
+      return _slideUp(target, duration);
+    }
+  }
+};
